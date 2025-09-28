7c8da357f7f1ecf6ada937417b20ce88604df74c
Ushio-Hayase <ushiohayase@gmail.com>
2025-09-28 17:17:17 +0900

    fix: add csharp class practice

---
+++ csharp/Method.cs
@@ -0,0 +1,78 @@
+﻿public abstract class Parent
+{
+    protected string _name;
+
+    public abstract void Method();
+}
+
+
+public class Child1 : Parent
+{
+    private string id_;
+
+    protected int x { get; set; }
+
+    public Child1()
+    {
+        id_ = "ds";
+        base._name = "112";
+    }
+
+    ~Child1()
+    {
+
+    }
+
+
+    public sealed override void Method()
+    {
+        Console.WriteLine("1");
+    }
+
+    public virtual void Method2()
+    {
+    }
+
+    public class Child1_1
+    {
+        public int y { get; set; }
+    }
+}
+
+public class Child2 : Parent
+{
+
+    public Child2()
+    {
+        base._name = "112";
+    }
+
+    ~Child2()
+    {
+
+    }
+
+
+    public override void Method()
+    {
+        Console.WriteLine("2");
+    }
+}
+
+
+public class Test
+{
+
+    public static int Main(string[] args)
+    {
+        Parent p = new Child1();
+
+        p.Method();
+
+        p = new Child2();
+
+        p.Method();
+
+        return 0;
+    }
+}
