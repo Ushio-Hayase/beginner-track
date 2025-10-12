07b7f52cac51d977a91494d7f0dd8ad05b698449
UshioHayase <UshioHayase@gmail.com>
2025-10-12 19:47:08 +0900

    feat: add collision

--- unity/Assets/Script/GameManager.cs
+++ unity/Assets/Script/GameManager.cs
@@ -4,13 +4,24 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject talkPanel;
     public Text talkText;
     public GameObject scanObject;
+    public bool isAction { get; }
 
     public void Action(GameObject scanObj)
     {
-        scanObject = scanObj;
-        talkText.text = "This name is " + scanObj.name;
+        if (isAction)
+        {
+            isAction = false;
+        }
+        else
+        {
+            isAction = true;
+            scanObject = scanObj;
+            talkText.text = "This name is " + scanObj.name;
+        }
+        talkPanel.SetActive(isAction);
     }
     
     void Start()
--- unity/Assets/Script/PlayerAction.cs
+++ unity/Assets/Script/PlayerAction.cs
@@ -9,11 +9,16 @@ public class PlayerAction : MonoBehaviour
     private float _h;
     private float _v;
 
+    private Vector3 _direction;
+
+    private GameObject _collisionObj;
+    
     private Rigidbody2D _rigid;
 
     private bool _isHorizonMove;
 
-    public GameObject gameManager;
+    public GameManager gameManager;
+    public Animator _anim;
 
     private void Awake()
     {
@@ -23,22 +28,49 @@ public class PlayerAction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!gameManager.isAction)
+        {
+            // Move
+            _h = Input.GetAxisRaw("Horizontal");
+            _v = Input.GetAxisRaw("Vertical");
+
+            // Check button up & down
+            bool hDown = Input.GetButtonDown("Horizontal");
+            bool vDown = Input.GetButtonDown("Vertical");
+
+            // Check horizontal move
+            if (hDown) _isHorizonMove = true;
+            else if (vDown) _isHorizonMove = false;
+
+            //Animation
+            if (_isHorizonMove)
+            {
+                _anim.SetInteger("hAxisRaw", (int)_h);
+            }
+            else
+                _anim.SetInteger("vAxisRaw", (int)_v);
+
+            //Direction
+            if (!_isHorizonMove && _v > 0) _direction = Vector3.up;
+            else if (!_isHorizonMove && _v < 0) _direction = Vector3.down;
+            else if (_isHorizonMove && _h > 0) _direction = Vector3.right;
+            else if (_isHorizonMove && _h < 0) _direction = Vector3.left;
+        }
         
-        _h = Input.GetAxisRaw("Horizontal");
-        _v = Input.GetAxisRaw("Vertical");
-
-        bool hDown = Input.GetButtonDown("Horizontal");
-        bool vDown = Input.GetButtonDown("Vertical");
-        bool hUp = Input.GetButtonUp("Horizontal");
-        bool vUp = Input.GetButtonUp("Vertical");
-
-        if (hDown || vUp) _isHorizonMove = true;
-        else if (vDown || hUp) _isHorizonMove = false;
+        if(Input.GetButtonDown("Jump")&&_collisionObj) gameManager.Action(_collisionObj);
     }
 
     void FixedUpdate()
     {
+        // Move
         Vector2 moveVec = _isHorizonMove ? new Vector2(_h, 0) : new Vector2(0, _v);
         _rigid.linearVelocity = moveVec * speed;
+        
+        // Ray
+        Debug.DrawRay(_rigid.position, _direction*0.7f, new Color(0,1,0));
+        RaycastHit2D rayHit = Physics2D.Raycast(_rigid.position, _direction, 0.7f, LayerMask.GetMask("Object"));
+
+        if (rayHit.collider) _collisionObj = rayHit.collider.gameObject;
+        else _collisionObj = null;
     }
 }
